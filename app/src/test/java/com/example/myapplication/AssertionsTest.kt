9d24f472c3544be2dfb00005b96e4e40bc708e45
Ernestom099 <ernestom099@outlook.com>
2022-08-31 15:53:31 -0500

    Usign assertEquals and assertNotEquals

--- app/src/test/java/com/example/myapplication/AssertionsTest.kt
+++ app/src/test/java/com/example/myapplication/AssertionsTest.kt
@@ -8,8 +8,17 @@ class AssertionsTest {
     @Test
     fun getArrayTest() {
         val assertions = Assertions()
-        val array = arrayOf(2, 71)//valor esperado
-        //val array = arrayOf(2, 7)//valor esperado
+        //val array = arrayOf(2, 71)//valor esperado
+        val array = arrayOf(2, 7)//valor esperado
         assertArrayEquals("mensaje personalizado de error en testing",array, assertions.getLuckyNumbers())
+    }
+
+    @Test
+    fun getNameTest(){
+        val assertions = Assertions()
+        val name = "Luis"
+        val otherName = "Juan"
+        assertEquals(name,assertions.getName())
+        assertNotEquals(otherName,assertions.getName())
     }
 }
